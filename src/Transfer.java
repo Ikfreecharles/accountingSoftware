e927c57292c81e37124e6c864ec92745bbc9d18d
omran <omranrashidy@gmail.com>
2020-12-06 20:45:33 +0100

    accounting software

---
+++ src/Transfer.java
@@ -0,0 +1,16 @@
+public class Transfer  {
+
+
+    private Transaction transaction = new Transaction();
+
+    public Transfer() {
+
+    }
+    // we need two methods to show transfer out.
+    // we need a method to transfer in.
+
+
+
+
+
+}
